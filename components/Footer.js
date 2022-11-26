1d642ccd374e3dcef25183e8ae81bce5686c916e
Khaleel <khaleelmusleh@gmail.com>
2022-11-25 16:40:14 -0800

    copyright

--- components/Footer.js
+++ components/Footer.js
@@ -22,9 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <Link href="https://authntech.com">AUTHNTECK © 2022</Link>
         </div>
       </div>
     </footer>
